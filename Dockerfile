ca86f5e2cd0b66722e4330f1a14ead6a1023fdc4
eddkentuchi <ediarpoll@gmail.com>
2024-08-09 02:40:38 +0000

    feat: run entrypont to postgresql more efficiently and variables

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,7 @@ FROM arm64v8/postgres:15
 # Eliminar el contenido del directorio de datos antes de iniciar
 RUN rm -rf /var/lib/postgresql/data/*
 # Copiar archivo de backup
-COPY sie_backup.sql /docker-entrypoint-initdb.d/
-# Copiar el script de creación de base de datos
-COPY init.sql /docker-entrypoint-initdb.d/
-#Copiar el cambio de nombre de la base de datos
-COPY zfinish.sql /docker-entrypoint-initdb.d/
+COPY sie_backup.sql /usr/local/bin/
 #Copiar script bash
 COPY entrypoint.sh /usr/local/bin/
 RUN chmod +x /usr/local/bin/entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,22 @@
 set -e
 echo "INICIA CONFIGURACIÓN"
-echo "Esperando 10 segundos..."
-sleep 2
 # Ejecutar el script de inicialización original de PostgreSQL
 if [ -s "$PGDATA/PG_VERSION" ]; then
   echo "PostgreSQL cluster already exists. Skipping initialization."
-  rm -r /docker-entrypoint-initdb.d
-  /usr/local/bin/docker-entrypoint.sh postgres 
+  /usr/local/bin/docker-entrypoint.sh postgres
 else
   echo "Initializing PostgreSQL cluster..."
   #ls -la /var/lib/postgresql/data
-  /usr/local/bin/docker-entrypoint.sh postgres 
-fi
+  /usr/local/bin/docker-entrypoint.sh postgres &
+echo "Cargando configuración inicial"
+sleep 10
 #echo "Termina configuración de postgresql"
-
-  # Esperar a que PostgreSQL esté listo
-  echo "Iniciando postgresql"
-#until pg_isready -h localhost -p 5432 -U postgres; do
-#  echo "Waiting for PostgreSQL to be ready..."
-#  sleep  2 
-#done
-#fi
+# 1. Crear la base de datos 'sie'
+psql -U postgres -c "CREATE USER $POSTGRES_USER  WITH LOGIN CREATEDB PASSWORD '$POSTGRES_USER_PASSWORD';"
+psql -U $POSTGRES_USER -d postgres -c "CREATE DATABASE $POSTGRES_DATABASE;"
+echo "Se creo la base de datos sie"
+# 2. Hacer el backup en sie mediante users
+pg_dump -U usertosie -d postgres -Fc sieusers > /usr/local/bin/sie_backup.sql
+echo "Finaliza Configuración"
+fi
+wait
